10ee187cbc05be5dbc68b837c3627d555ff819bf
dmitriyShatalin19 <shatal1d@yandex.ru>
2026-01-21 12:07:29 +0300

    20/01/2026

---
+++ script.js
@@ -0,0 +1,40 @@
+// задчача 1: Фильтрация пользователей по возрасту
+// function getAdultUsers() {
+//     const users = [
+//     {name: 'Анна', age: 25},
+//     {name: 'Борис', age: 17},
+//     {name: 'Вера', age: 30},
+//     {name: 'Глеб', age: 16},
+//     {name: 'Дарья', age: 22},
+//     ];
+
+//     const adults = [];
+
+//     for (let index = 0; index <= users.length - 1; index++) {
+//         let element = users[index];
+//         if (element.age >= 18) {
+//             adults.push(element);
+//         }
+//     }
+//     return adults;
+// }
+// console.log(getAdultUsers());
+
+// задача 2: Подсчет сумыы четных чисел
+// function sumEvenNumbers() {
+//     const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//     let sum = 0;
+
+//     for (let index = 0; index < numbers.length; index++) {
+//         const element = numbers[index];
+//         if (element % 2 === 0) {
+//             sum = sum + element;
+//         }
+//     }
+//     return sum;
+// }
+// console.log(sumEvenNumbers());
+
+
+
+
