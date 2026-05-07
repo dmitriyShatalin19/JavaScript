6db38f677ab25f3de86d24a8509c1bba89179135
Dima <shatal1d@yandex.ru>
2026-05-07 11:04:44 +0300

    повторил условия, лог. операторы  и switch

--- OneDrive/Desktop/javaScript/script.js
+++ OneDrive/Desktop/javaScript/script.js
@@ -1,3 +1,91 @@
+// задачи для повторение
+// задача 1.
+// const age = 17;
+// if (age >= 18) {
+//     console.log('доступ открыт');
+// } else {
+//     console.log('доступ закрыт');
+// }
 
+// задача 2.
+// const isLoggedIn = false;
+// const isAdmin = true;
+// if (isLoggedIn === true && isAdmin === true) {
+//     console.log('привет, админ');
+// } else if (isLoggedIn === false && isAdmin === true) {
+//     console.log('гость');
+// } else if (isLoggedIn === true && isAdmin === false) {
+//     console.log('пользователь');
+// } else {
+//     console.log('нет аккаунта');
+// }
 
+// задача 3.
+// const day = 5;
+// switch (day) {
+//     case 1: {
+//         console.log('понидельник');
+//         break;
+//     };
+//     case 2: {
+//         console.log('вторник');
+//         break;
+//     };
+//     case 3: {
+//         console.log('среда');
+//         break;
+//     };
+//     case 4: {
+//         console.log('четверг');
+//         break;
+//     };
+//     case 5: {
+//         console.log('пятница');
+//         break;
+//     };
+//     case 6: {
+//         console.log('суббота');
+//         break;
+//     };
+//     case 7: {
+//         console.log('воскресенье');
+//         break;
+//     }
+// }
 
+// задача 4.
+// const num = 2;
+// if (num >= 10 && num <= 20 || num === 100) {
+//     console.log('подходит');
+// } else {
+//     console.log('не подходит');
+// };
+
+// задача 5.
+// const month = 12;
+// switch (month) {
+//     case 12:
+//     case 1:
+//     case 2: {
+//         console.log('зима');
+//         break;
+//     };
+//     case 3: 
+//     case 4: 
+//     case 5: {
+//         console.log('весна');
+//         break;
+//     };
+//     case 6: 
+//     case 7: 
+//     case 8: {
+//         console.log('лето');
+//         break;
+//     };
+//     case 9:
+//     case 10:
+//     case 11: {
+//         console.log('осень');
+//         break;
+//     };
+// };
