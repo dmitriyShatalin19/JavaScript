e013b64bd1ad5ea9fe8fa88584657f95a095efa8
Dima <shatal1d@yandex.ru>
2026-05-08 22:19:37 +0300

    повторял циклы и условия

---
+++ script.js
@@ -0,0 +1,37 @@
+// задание 1.
+// function countToN(n) {
+//     for (let i = 1; i <= n; i++) {
+//         console.log(i);
+//     }
+// }
+// console.log(countToN(5))
+
+// задача 2. 
+// function countdown(start) {
+//     while (start > 0) {
+//         start--;
+//         console.log(start);
+//     }
+//     return 'поехали!';
+// }
+// console.log(countdown(5))
+
+// задача 3.
+// function sumFromToB(a, b) {
+//     let sum = 0;
+//     for (let i = a; i <= b; i++) {
+//         sum+=i;
+//     }
+//     return sum;
+// }
+// console.log(sumFromToB(1, 5));
+
+// задача 4.
+// function findFirstDivsible(x, y) {
+//     let number = 1;
+//     while (number % x !== 0 || number % y !== 0) {
+//         number++;
+//     }
+//     return number;
+// }
+// console.log(findFirstDivsible(2, 3))
