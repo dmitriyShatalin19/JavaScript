4da1d78a32d3855f81cf162d37f79d986fd4b822
Dima <shatal1d@yandex.ru>
2026-05-10 17:17:06 +0300

    все еще повторяю

--- script.js
+++ script.js
@@ -94,3 +94,70 @@
 //     sum = n * i;
 //     console.log(sum)
 // }
+
+// задача 11.
+// let num = 5;
+// if (num >= 1 && num <= 10) {
+//     console.log('число в диапазоне');
+// } else {
+//     console.log('число вне диапазона');
+// }
+
+// задача 12.
+// const login = 'admin';
+// const password = '1234';
+// if (login === 'admin' && password === '12345') {
+//     console.log('Выход выполнен');
+// } else {
+//     console.log('Ошбика выхода');
+// }
+
+// задача 13.
+// const a = 22;
+// const b = 22;
+// if (a > b) {
+//     console.log(a);
+// } else if (a < b) {
+//     console.log(b)
+// } else {
+//     console.log('Числа равны');
+// }
+
+// задача 14.
+// const score = 10;
+// if (score >= 90 && score <= 100) {
+//     console.log('Отлично');
+// } else if (score >= 70 && score <= 89) {
+//     console.log('Хорошо');
+// } else if (score >= 50 && score <= 69) {
+//     console.log('Удовлетворительно');
+// } else {
+//     console.log('не сдал');
+// }
+
+// задача 15.
+// let sum = 0;
+// for (let i = 1; i <= 50; i++) {
+//     if (i % 2 === 0) {
+//         sum++;
+//     }
+// }
+// console.log(sum)
+
+// задача 16.
+// let sum = 0;
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 2 !== 0) {
+//         sum++;
+//     }
+// }
+// console.log(sum)
+
+// задача 17.
+// let sum = 0;
+// for (let i = 1; i <= 30; i++) {
+//     if (i % 3 === 0) {
+//         sum++;
+//     }
+// }
+// console.log(sum)
